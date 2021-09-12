34c820ee71b524fcd476f85d7e684413ca8019c1
vladimir <vladimirleonov789@gmail.com>
2021-09-12 20:12:35 +0300

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const inp = document.querySelector('#page-number');
 
-const btn = document.querySelector('.button');
+const btnGetImages = document.querySelector('.button__get-images');
 
-btn.addEventListener('click', () => {
+btnGetImages.addEventListener('click', () => {
    getImages(inp.value)
-      .then(onDataReceived);
+      .then(onImagesReceived);
 });
 
 
-function onDataReceived (data) {
+function onImagesReceived (data) {
    console.log(data);
    data.forEach(element => {
       const img = document.createElement('img');
       img.src = element.original;
       img.style.cssText = `width: 150px`;
-      document.querySelector('.result').appendChild(img);
+      document.querySelector('.result__images').appendChild(img);
    });
 }
+
+const btnGetTasks = document.querySelector('.button__get-tasks');
+btnGetTasks.addEventListener('click', () => {
+   getTasks()
+      .then(onTasksReceived);
+});
+
+function onTasksReceived(data) {
+   data.forEach(task => {
+      const li = document.createElement('li');
+      // debugger;
+      li.innerHTML = task.title;
+      document.querySelector('.result__tasks').appendChild(li);
+   });
+}
+
+createTask('Learn programming');
+// createTask('Learn English');
+// createTask('Learn English');
+// createTask('Learn English');
--- service.js
+++ service.js
@@ -11,3 +11,27 @@ function getImages(imagePage) {
 //    const promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${imagePage}&count=3`);
 //    return promise;   
 // }
+
+
+
+//путь для api todo
+//widgetId- обязательный параметр(по документации у Димыча)
+// https://repetitora.net/api/JS/Tasks?widgetId=12345 
+
+
+function getTasks() {
+   const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=21`);
+   return promise.then(response => {
+      return (response.data);
+   });
+}
+
+function createTask(title) {
+   const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+      widgetId: "21",
+      title: title
+   });
+   return promise.then((response) => {
+      return (response.data);
+   });
+}
